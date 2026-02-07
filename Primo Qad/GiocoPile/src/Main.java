365e7efbe33b41b2bc7a920ac64bb7ae622638a9
CeleghinRiccardo <celeghin.riccardo@studenti.salesianisesto.it>
2026-02-07 12:37:11 +0100

    [CASA]

--- Primo Qad/GiocoPile/src/Main.java
+++ Primo Qad/GiocoPile/src/Main.java
@@ -1,5 +1,14 @@
 public class Main {
-    public static void main (String[]args){
+    public static void main(String[] args) {
+
+        Pila<Blocco> pila1= new Pila<>();
+        Pila<Blocco> pila2= new Pila<>();
+        Pila<Blocco> pila3= new Pila<>();
+        Blocco b1 = new Blocco(1);
+        Blocco b2 = new Blocco(2);
+        Blocco b3 = new Blocco(3);
+
+
 
     }
 }
---
+++ Primo Qad/GiocoPile/src/Pila.java
@@ -0,0 +1,50 @@
+import java.util.ArrayList;
+
+public class Pila <Blocco>{
+
+    private ArrayList<Blocco> Pila;
+    private int i;
+
+    public Pila(){
+        Pila = new ArrayList<>();
+    }
+
+    public void push(Blocco obj){
+        Pila.add(obj);
+    }
+
+    public Blocco pop(){
+        if(Pila.size()>0){
+            return Pila.remove(Pila.size()-1);
+        }
+        return null;
+    }
+
+    public Blocco top(){
+        if(Pila.size()>0){
+            return Pila.getLast();
+        }
+        return null;
+    }
+
+    public int size(){
+        return Pila.size();
+    }
+
+    public boolean vuota(){
+        if(Pila.size()>0){
+            return false;
+        }
+        return true;
+    }
+
+    public void svuota(){
+        Pila.clear();
+    }
+
+    @Override
+    public String toString() {
+        return Pila.toString();
+    }
+
+}
